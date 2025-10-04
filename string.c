dea319c9abcf3d24985d5ca10a4ec7792248182b
M.Matin Monajemi <81869332+MmatinMonajemi@users.noreply.github.com>
2025-10-04 19:56:45 +0330

    Create string.c

---
+++ string.c
@@ -0,0 +1,22 @@
+#include "string.h"
+
+int matin_strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++; s2++;
+    }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+int matin_strncmp(const char* s1, const char* s2, size_t n) {
+    while (n && *s1 && (*s1 == *s2)) {
+        s1++; s2++; n--;
+    }
+    if (n == 0) return 0;
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
+
+size_t matin_strlen(const char* str) {
+    size_t len = 0;
+    while (str[len] != '\0') len++;
+    return len;
+}
